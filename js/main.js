e40eff2a33fd89ac1757726d42136e91c9727c4c
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-02-02 16:03:54 +0900

    Remove fadeIn effect

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 'use strict';
 import 'babel-polyfill';
-import loadingComplete from './loadingComplete';
 import fixFlexbox from './fixFlexBox';
 import createFuture from './createFuture';
-import showCotents from './showContents';
 
 const init = () => {
-  loadingComplete();
   fixFlexbox();
   createFuture();
-  showCotents();
 };
 
 window.addEventListener('load', init);
