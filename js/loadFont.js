f8be02876a139aeb092142a7140bac7f94e7de7b
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-02-07 18:27:01 +0900

    Add font fouc

---
+++ js/loadFont.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export default function () {
+  const className = 'junge';
+  document.body.classList.add(className);
+}
--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 import 'babel-polyfill';
 import fixFlexbox from './fixFlexBox';
 import createFuture from './createFuture';
+import loadFont from './loadFont';
 
 const init = () => {
   fixFlexbox();
   createFuture();
+  loadFont();
 };
 
 window.addEventListener('load', init);
