851e24c5981dc3bdbbb5a5095be922dffa67b175
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-04-26 21:47:01 +0900

    fix delay-load-images

--- src/js/delay-load-images.js
+++ src/js/delay-load-images.js
@@ -1,20 +1,22 @@
 'use strict';
 
 const delayLoadImages = () => {
-  const images = document.querySelectorAll('img[data-src]:not([src])');
+  const images = document.querySelectorAll('img[data-src]');
 
-  Array.from(images).forEach(image => {
-    const preloader = new Image();
-    const {src} = image.dataset;
+  Array.from(images)
+    .filter(image => image.dataset.src !== image.src)
+    .forEach(image => {
+      const preloader = new Image();
+      const {src} = image.dataset;
 
-    image.style.opacity = 0;
-    preloader.addEventListener('load', () => {
-      image.src = src;
-      image.style.transition = 'opacity .8s';
-      image.style.opacity = '';
+      image.style.opacity = 0;
+      preloader.addEventListener('load', () => {
+        image.src = src;
+        image.style.transition = 'opacity .8s';
+        image.style.opacity = '';
+      });
+      preloader.src = src;
     });
-    preloader.src = src;
-  });
 };
 
 module.exports = delayLoadImages;
