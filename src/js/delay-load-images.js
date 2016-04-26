34c99d2feec0209c20d1b479663493cfc3615b33
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-04-27 01:39:11 +0900

    add timeout to delay load images

--- src/js/delay-load-images.js
+++ src/js/delay-load-images.js
@@ -1,22 +1,36 @@
 'use strict';
 
+const delayLoadImage = (image, index = 0) => {
+  const interval = 500;
+  const {src} = image.dataset;
+  let timeout = null;
+  let isCalled = false;
+  const loadImage = () => {
+    if (isCalled) {
+      return;
+    }
+
+    image.src = src;
+    image.style.transition = 'opacity .8s ease';
+    image.style.opacity = '';
+
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+    isCalled = true;
+  };
+  const preloader = new Image();
+
+  image.style.opacity = 0;
+  preloader.addEventListener('load', loadImage);
+  preloader.src = src;
+  timeout = setTimeout(loadImage, interval * (index + 1));
+};
+
 const delayLoadImages = () => {
   const images = document.querySelectorAll('img[data-src]');
-
-  Array.from(images)
-    .filter(image => image.dataset.src !== image.src)
-    .forEach(image => {
-      const preloader = new Image();
-      const {src} = image.dataset;
-
-      image.style.opacity = 0;
-      preloader.addEventListener('load', () => {
-        image.src = src;
-        image.style.transition = 'opacity .8s';
-        image.style.opacity = '';
-      });
-      preloader.src = src;
-    });
+  Array.from(images).forEach(delayLoadImage);
 };
 
 module.exports = delayLoadImages;
